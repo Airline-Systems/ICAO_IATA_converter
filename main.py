d281351ec41ca02e80b81de0c807043445fbad0b
Airline Systems <83115512+Airline-Systems@users.noreply.github.com>
2021-04-26 17:56:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
 from flask import Flask, request, render_template, url_for, redirect, jsonify
 from flask_restful import Api, Resource, reqparse
-from converter import airport
+from converter import *
+import csv
 
 app = Flask(__name__)
 api = Api(app)
 app.config['JSON_SORT_KEYS'] = False
 
-@app.route('/<icao>', methods=['GET'])
-def convert(icao):
-    iata = airport(icao)
-    #return iata
+
+@app.route('/icao_to_iata/<icao>', methods=['GET'])
+def IcaoToIata(icao):
+    csv_file = csv.reader(open('./icao_iata.csv', 'r'))
+
+    #find the value in the relevant csv and return an IATA code
+    if len(icao) == 4:
+        for row in csv_file:
+            if icao in row[0]:
+                iata = ''.join(row)
+                iata = iata[5:8]
+
+    else:
+        iata="error"
+        icao="this is not a valid 4-letter airport code"
     return jsonify({icao: iata})
+
+@app.route('/iata_to_icao/<iata>', methods=['GET'])
+def IataToIcao(iata):
+    csv_file = csv.reader(open('./iata_icao.csv', 'r'))
+    #find the value in the relevant csv and return an ICAO code
+    if len(iata)==3:
+        for row in csv_file:
+            if iata in row[0]:
+                icao = ''.join(row)
+                icao = icao[4:8]
+    else:
+        iata = "this is not a valid 3 letter airport code"
+        icao = "error"
+
+    return jsonify({iata: icao})
 
 if __name__ == "__main__":
     app.run(debug=True)
